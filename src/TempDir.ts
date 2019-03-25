957963f45c2f3a0404c7a6c124ba9b4abcdc849c
eran shabi <eranshabi@gmail.com>
2019-03-25 18:40:44 +0200

    add testing infra

---
+++ src/TempDir.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+import path from "path";
+import tmp from "tmp";
+import rimraf from "rimraf";
+
+export class TempDir {
+  private dir: { name: string; removeCallback: Function };
+
+  constructor() {
+    this.dir = tmp.dirSync();
+    this.setup({
+      "package.json": JSON.stringify({
+        name: "test",
+        jest: {
+          collectCoverage: true
+        }
+      })
+    });
+  }
+
+  public destroy(): void {
+    this.checkDir();
+    rimraf.sync(this.getPath());
+    this.dir = null;
+  }
+
+  public getPath(): string {
+    this.checkDir();
+    return this.dir.name;
+  }
+
+  public setup(files: { [key: string]: string }): void {
+    this.checkDir();
+    Object.keys(files).forEach(fileName => {
+      fs.writeFileSync(
+        path.join(this.getPath(), fileName),
+        files[fileName],
+        "utf8"
+      );
+    });
+  }
+
+  public read(files: string[]): { [key: string]: string } {
+    this.checkDir();
+    return Object.assign(
+      {},
+      ...files.map(fileName => ({
+        [fileName]: fs.readFileSync(path.join(this.getPath(), fileName), "utf8")
+      }))
+    );
+  }
+
+  public readFile(fileName: string): string {
+    this.checkDir();
+    return fs.readFileSync(path.join(this.getPath(), fileName), "utf8");
+  }
+
+  private checkDir() {
+    if (this.dir === null) {
+      throw new Error("The dir is destroyed");
+    }
+  }
+}
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,53 @@
+import { runCLI } from "@jest/core";
+import { Config } from "@jest/types";
+import { TempDir } from "./TempDir";
+
 describe("", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+  let tempDir;
+  beforeEach(() => {
+    tempDir = new TempDir();
+  });
+  afterEach(() => {
+    tempDir.destroy();
+  });
+
+  it("should pass", async () => {
+    const file = `
+            module.exports = function a() {
+                console.log(2);
+                if (false) {
+                    console.log(1);
+            
+                }
+            };
+        `;
+
+    const testFile = `
+            const a = require('./codeFile.js');
+            
+            
+            describe('', () => {
+                it('should', () => {
+                    a();
+                    expect(true).toBe(true)
+                });
+            });
+  
+        `;
+
+    tempDir.setup({
+      "codeFile.js": file,
+      "codeFile.spec.js": testFile
+    });
+
+    const { results } = await runCLI(
+      {
+        collectCoverage: true,
+        silent: true,
+        outputFile: "1.txt"
+      } as Config.Argv,
+      [tempDir.getPath()]
+    );
+    expect(Object.keys(results.coverageMap.data)[0]).toContain("codeFile.js");
   });
 });
